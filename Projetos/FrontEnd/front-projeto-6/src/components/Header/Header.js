4397e43d633a4eed565a0251bf78aa58570f38a9
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-07-11 00:19:48 -0300

    Creating the Dark Mode Button with Animation

--- Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.js
+++ Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.js
@@ -1,11 +1,57 @@
+import { useState } from 'react';
 import { Typography } from '@material-ui/core';
-import { Spacing, StyledHeader } from './Header.styled';
+import Lottie from 'react-lottie';
+import animationData from './Animation.json';
+import {
+  StyledHeader,
+  Spacing,
+  AnimationButton,
+  AnimationGrid,
+} from './Header.styled';
 
 export function Header() {
+  const [animationDarkMode, setAnimationDarkMode] = useState({
+    isStopped: true,
+    isPaused: false,
+    direction: -1,
+  });
+  const defaultOptions = {
+    loop: false,
+    autoplay: false,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
   return (
     <>
       <StyledHeader>
         <Typography>Jboys - Consulta de Processos</Typography>
+        <AnimationButton
+          onClick={() => {
+            const reverseAnimation = -1;
+            const normalAnimation = 1;
+            setAnimationDarkMode({
+              ...animationDarkMode,
+              isStopped: false,
+              direction:
+                animationDarkMode.direction === normalAnimation
+                  ? reverseAnimation
+                  : normalAnimation,
+            });
+          }}
+        >
+          <AnimationGrid>
+            <Lottie
+              options={defaultOptions}
+              height={50}
+              width={50}
+              direction={animationDarkMode.direction}
+              isStopped={animationDarkMode.isStopped}
+              isPaused={animationDarkMode.isPaused}
+            />
+          </AnimationGrid>
+        </AnimationButton>
       </StyledHeader>
       <Spacing />
     </>
--- Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.styled.js
+++ Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.styled.js
@@ -6,10 +6,34 @@ const headerHeight = 6;
 const StyledHeader = styled(AppBar)(({ theme }) => ({
   background: theme.palette.primary.main,
   height: theme.spacing(headerHeight),
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: theme.spacing(2),
 }));
 
 const Spacing = styled('div')(({ theme }) => ({
   height: theme.spacing(headerHeight),
 }));
 
-export { StyledHeader, Spacing };
+const AnimationButton = styled('button')(({ theme }) => ({
+  width: '40px',
+  height: '30px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontSize: '33px',
+  backgroundColor: 'transparent',
+  border: '0',
+  padding: '0',
+  cursor: 'pointer',
+  outline: '0',
+  borderRadius: '100%',
+}));
+
+const AnimationGrid = styled('div')(({ theme }) => ({
+  pointerEvents: 'none',
+}));
+
+export { StyledHeader, Spacing, AnimationButton, AnimationGrid };
