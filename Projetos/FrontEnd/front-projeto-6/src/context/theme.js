56a645cad30cc351e634ebca917310319355b294
LucasPires <74467549+Notnox@users.noreply.github.com>
2021-07-12 21:48:57 -0300

    Setting localStorage for darkMode

--- Projetos/FrontEnd/front-projeto-6/src/context/theme.js
+++ Projetos/FrontEnd/front-projeto-6/src/context/theme.js
@@ -3,6 +3,9 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { getTheme } from "theme/themes";
 
+//  const LOCAL_STORAGE_DARKMODE = process.env.REACT_APP_DARK_THEME; Está retornando undefined, verificar depois!!
+const LOCAL_STORAGE_DARKMODE = "darkMode";
+
 export const ThemesContext = createContext();
 
 const ThemesProvider = ({ children }) => {
@@ -14,7 +17,16 @@ const ThemesProvider = ({ children }) => {
   }
 
   useEffect(() => {
+    const darkModeStorage = localStorage.getItem(LOCAL_STORAGE_DARKMODE);
+
+    if (darkModeStorage) {
+      setDarkMode(darkModeStorage === "true");
+    }
+  }, []);
+
+  useEffect(() => {
     setTheme(getTheme(darkMode));
+    localStorage.setItem(LOCAL_STORAGE_DARKMODE, darkMode);
   }, [darkMode]);
 
   return (
@@ -27,6 +39,13 @@ const ThemesProvider = ({ children }) => {
   );
 };
 
-export { ThemesProvider };
+function useThemes() {
+  const context = useContext(ThemesContext);
 
-export const useThemes = () => useContext(ThemesContext);
+  if (!context) {
+    throw new Error("useThemes must be involved by ThemesProvider!");
+  }
+  return context;
+}
+
+export { ThemesProvider, useThemes };
