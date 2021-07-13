e669950166daba51d0f330ef394fa709e485d2d9
Michael Nascimento <michael.nsc@outlook.com>
2021-07-13 20:13:27 -0300

    Estrutura basica de estilização (para exemplo)

--- Projetos/frontend/src/components/AppBar/AppBar.js
+++ Projetos/frontend/src/components/AppBar/AppBar.js
@@ -1,4 +1,8 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
+import style from './AppBar.style';
+
+const useStyle = makeStyles(style);
 
 export default function AppBar() {
   return <div>Ola</div>;
