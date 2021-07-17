f10d1c576d4b11b1a11068696f638093c52827c9
thiagomathiassimon <mathiastsimon@gmail.com>
2021-07-17 20:27:04 -0300

    WIP: Tela de Cadastro de Processo.

--- Projetos/frontend/src/App.js
+++ Projetos/frontend/src/App.js
@@ -1,11 +1,13 @@
 import { process } from './mock';
 import { BaseLayout } from './layouts/BaseLayout';
 import { ListProcessPage } from './pages/ListProcessPage';
+import { ProcessRegistrationPage } from './pages/ProcessRegistrationPage';
 
 export const App = () => {
   return (
     <BaseLayout>
-      <ListProcessPage process={process} />
+      {/*<ListProcessPage process={process} />*/}
+      <ProcessRegistrationPage/>
     </BaseLayout>
   );
 };
---
+++ Projetos/frontend/src/pages/ProcessRegistrationPage/ProcessRegistrationPage.styles.js
@@ -0,0 +1,32 @@
+import { makeStyles } from '@material-ui/core';
+
+export const useStyles = makeStyles((theme) => ({
+  title: {
+    marginTop: 20,
+    fontSize: 'XX-large',
+    color: '#878788',
+  },
+  container: {
+    width: 700,
+    minHeight: 700,
+    marginBottom: 32,
+    paddingBottom: 25,
+  },
+  form: {
+    width: 600,
+  },
+  thirdInput: {
+    width: '30%',
+  },
+  halfInput: {
+    width: '47.5%',
+  },
+  divider: {
+    marginTop: 15,
+  },
+  subtitle: {
+    marginTop: 20,
+    fontSize: 'X-large',
+    color: '#878788',
+  },
+}));
