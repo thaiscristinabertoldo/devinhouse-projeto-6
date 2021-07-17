abc8bb20e9e77d675842ff8ea92b5b76504d0f94
Michael Nascimento <michael.nsc@outlook.com>
2021-07-17 16:39:36 -0300

    Usa LoginPage no App

--- Projetos/frontend/src/App.js
+++ Projetos/frontend/src/App.js
@@ -1,11 +1,14 @@
 import { process } from './mock';
 import { BaseLayout } from './layouts/BaseLayout';
 import { ListProcessPage } from './pages/ListProcessPage';
+import { LoginPage } from './pages/LoginPage';
 
 export const App = () => {
   return (
     <BaseLayout>
-      <ListProcessPage process={process} />
+      {/*<ListProcessPage process={process} />*/}
+      <LoginPage />
     </BaseLayout>
+
   );
 };
