ec937a99ac7b120bd42cab107d02b577952f0377
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2021-07-19 20:08:36 -0300

    refactor: header

--- Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.js
+++ Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.js
@@ -1,34 +1,34 @@
-import { useTheme } from "@emotion/react";
-import { Typography, useMediaQuery } from "@material-ui/core";
-import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import DarkModeIcon from "@material-ui/icons/DarkMode";
-import LightModeIcon from "@material-ui/icons/LightMode";
-import { HeaderMenu } from "components/HeaderMenu";
-import { useThemes } from "context/theme";
-import { StyledHeader, Field, Spacing } from "./Header.styled";
+import { useContext } from 'react';
+import { Typography, IconButton } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
+import {
+  AccountBalance,
+  AccountCircle,
+  DarkMode,
+  LightMode,
+} from '@material-ui/icons';
+import { ThemeContext } from 'theme';
+import * as S from './Header.styled';
 
 export const Header = () => {
-  const { darkMode, ToggleTheme } = useThemes();
+  const { toggleTheme } = useContext(ThemeContext);
   const theme = useTheme();
-  const isLargeScreen = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <>
-      <StyledHeader>
-        <Field>
-          <AccountBalanceIcon style={{ marginRight: "8px" }} />
-          <Typography>{!isLargeScreen && "Jboys - Consultoria"}</Typography>
-        </Field>
-        <Field>
-          <HeaderMenu title={darkMode ? "LIGHT" : "DARK"} onClick={ToggleTheme}>
-            {darkMode ? <LightModeIcon /> : <DarkModeIcon />}
-          </HeaderMenu>
-          <HeaderMenu title="CONECTAR" onClick={null}>
-            <AccountCircleIcon />
-          </HeaderMenu>
-        </Field>
-      </StyledHeader>
-      <Spacing />
+      <S.Header>
+        <Typography>Jboys - Consultoria</Typography>
+
+        <IconButton onClick={toggleTheme}>
+          {theme?.palette?.mode === 'dark' ? <DarkMode /> : <LightMode />}
+        </IconButton>
+
+        <IconButton onClick={() => {}}>
+          <AccountCircle />
+        </IconButton>
+      </S.Header>
+
+      <S.Spacing />
     </>
   );
 };
--- Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.styled.js
+++ Projetos/FrontEnd/front-projeto-6/src/components/Header/Header.styled.js
@@ -1,27 +1,20 @@
-import { AppBar } from "@material-ui/core";
-import { styled } from "@material-ui/core/styles";
+import { AppBar } from '@material-ui/core';
+import { styled } from '@material-ui/core/styles';
 
 const headerHeight = 8;
 
-const StyledHeader = styled(AppBar)(({ theme }) => ({
+const Header = styled(AppBar)(({ theme }) => ({
   background: theme.palette.primary.dark,
   height: theme.spacing(headerHeight),
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  justifyContent: "space-between",
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
   padding: theme.spacing(3),
 }));
 
-const Field = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  padding: theme.spacing(1),
-}));
-
-const Spacing = styled("div")(({ theme }) => ({
+const Spacing = styled('div')(({ theme }) => ({
   height: theme.spacing(headerHeight),
 }));
 
-export { StyledHeader, Spacing, Field };
+export { Header, Spacing };
