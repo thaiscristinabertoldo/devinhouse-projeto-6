7206df27dcadf90d11579dea551d35d8f08f2d2f
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2021-07-19 20:07:56 -0300

    refactor: dark theme

--- Projetos/FrontEnd/front-projeto-6/src/index.jsx
+++ Projetos/FrontEnd/front-projeto-6/src/index.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { ThemesProvider } from "context/theme";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from 'App';
+import { ThemeProvider } from 'theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemesProvider>
+    <ThemeProvider>
       <App />
-    </ThemesProvider>
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById('root'),
 );
