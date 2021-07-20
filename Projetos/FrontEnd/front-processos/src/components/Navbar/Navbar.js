2464111b24ad1a98dca3f05e64ecb397fb0d87a5
Milena Diniz Vieira <miladvieira@outlook.com>
2021-07-19 21:06:37 -0300

    Instalado Material-ui

--- Projetos/FrontEnd/front-processos/src/components/Navbar/Navbar.js
+++ Projetos/FrontEnd/front-processos/src/components/Navbar/Navbar.js
@@ -1,10 +1,4 @@
-import {
-  AppBar,
-  Button,
-  IconButton,
-  Toolbar,
-  Typography
-} from '@material-ui/core'
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core'
 import { useStyles } from './Navbar.styles'
 import MenuIcon from '@material-ui/icons/Menu'
 
@@ -13,20 +7,15 @@ export const Navbar = () => {
 
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
+      <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-          >
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant='h6' className={classes.title}>
+          <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button color='inherit'>Login</Button>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
