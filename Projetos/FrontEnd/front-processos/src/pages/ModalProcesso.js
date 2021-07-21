2a1724a7ae03ca014cab62e66130b0295fe02c1d
Milena Diniz Vieira <miladvieira@outlook.com>
2021-07-21 14:13:20 -0300

    Modal em andamento

--- Projetos/FrontEnd/front-processos/src/pages/ModalProcesso.js
+++ Projetos/FrontEnd/front-processos/src/pages/ModalProcesso.js
@@ -1,5 +1,6 @@
 import { Modal } from '@material-ui/core'
 import { useStyles } from './ModalProcesso.styles'
+import { Grid, Paper, TextField } from '@material-ui/core'
 
 const ModalProcesso = ({ open, onClose }) => {
   const classes = useStyles()
@@ -7,7 +8,38 @@ const ModalProcesso = ({ open, onClose }) => {
   return (
     <Modal open={open} onClose={onClose} className={classes.modal}>
       <div className={classes.paper}>
-        <h2 id="transition-modal-title">Transition modal</h2>
+        <h2 id="transition-modal-title">Criando Processo</h2>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <TextField id="outlined-basic" label="Orgão Setor" variant="outlined" />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <TextField id="outlined-basic" label="Ano" variant="outlined" />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>
+              <TextField id="outlined-basic" label="Descrição" variant="outlined" />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     </Modal>
   )
