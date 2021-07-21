e74c6b4c30f09ae94aa46dce2a5e57ac28708ec5
Milena Diniz Vieira <miladvieira@outlook.com>
2021-07-20 21:22:58 -0300

    navBar com o theme

--- Projetos/FrontEnd/front-processos/src/components/Navbar/Navbar.js
+++ Projetos/FrontEnd/front-processos/src/components/Navbar/Navbar.js
@@ -1,12 +1,4 @@
-import {
-  AppBar,
-  Button,
-  IconButton,
-  Tooltip,
-  Toolbar,
-  Typography,
-  useTheme
-} from '@material-ui/core'
+import { AppBar, Button, IconButton, Tooltip, Toolbar, Typography, useTheme } from '@material-ui/core'
 import { useStyles } from './Navbar.styles'
 import MenuIcon from '@material-ui/icons/Menu'
 import Brightness4Icon from '@material-ui/icons/Brightness4'
@@ -19,21 +11,16 @@ export const Navbar = ({ toggleTheme }) => {
 
   return (
     <div className={classes.root}>
-      <AppBar position='static'>
+      <AppBar position="static" className={classes.appBar}>
         <Toolbar>
-          <IconButton
-            edge='start'
-            className={classes.menuButton}
-            color='inherit'
-            aria-label='menu'
-          >
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant='h6' className={classes.title}>
+          <Typography variant="h6" className={classes.title}>
             Tribunal de Justiça
           </Typography>
-          <Tooltip title='Alterar tema'>
-            <IconButton aria-label='delete' onClick={toggleTheme}>
+          <Tooltip title="Alterar tema">
+            <IconButton aria-label="delete" onClick={toggleTheme}>
               {palette?.type === 'dark' ? (
                 <Brightness4Icon className={classes.iconColor} />
               ) : (
@@ -41,7 +28,7 @@ export const Navbar = ({ toggleTheme }) => {
               )}
             </IconButton>
           </Tooltip>
-          <Button color='inherit'>Login</Button>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </div>
