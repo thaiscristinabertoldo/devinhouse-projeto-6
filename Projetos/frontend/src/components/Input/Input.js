cab4b47d918f7a6b0783af8158e6c3f41c0f6521
Michael Nascimento <michael.nsc@outlook.com>
2021-07-23 17:28:08 -0300

    Mergeing

--- Projetos/frontend/src/components/Input/Input.js
+++ Projetos/frontend/src/components/Input/Input.js
@@ -1,7 +1,13 @@
-import { TextField } from '@material-ui/core';
-import { useStyles } from './Input.styles';
+import { makeStyles, TextField } from '@material-ui/core';
 
 export const Input = (props) => {
   const classes = useStyles();
   return <TextField variant="outlined" inputProps={{ autoComplete: 'off' }} {...props} className={classes.input} />;
 };
+
+const useStyles = makeStyles((theme) => ({
+  input: {
+    marginTop: theme.spacing(1.5),
+    width: '100%',
+  },
+}));
